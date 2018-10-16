7ac0d3922a889dc5a65869fa04f2e1c48c6165ef
maestroprog <maestroprog@naver.com>
2018-10-17 02:00:59 +0900

    FlexBox in ReactNative
    syntex

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View  , ActivityIndicator} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
+        <View style={styles.redView}/>
+        <View style={styles.yellowView}/>
       </View>
     );
   }
@@ -15,7 +34,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    alignItems:'flex-end',
+    flexDirection: 'row',
+    flexWrap:'wrap'
   },
+  redView : {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red',
+    alignSelf: 'flex-start'
+  },
+  yellowView : {
+    height: 50,
+    width: 50,
+    backgroundColor: 'yellow',
+    alignSelf: 'center'
+  }
 });
